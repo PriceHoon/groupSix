9a85369593d82864f82883ca979a219c2a9ab93b
skyriv213 <gn1007@dankook.ac.kr>
2023-01-02 15:01:33 +0900

    refactor(response) change  Response Type - String to RestApiResponse

--- src/main/java/com/sparta/spartagroupsixproject/controller/LikeCommentController.java
+++ src/main/java/com/sparta/spartagroupsixproject/controller/LikeCommentController.java
@@ -1,12 +1,13 @@
 package com.sparta.spartagroupsixproject.controller;
 
 
+import com.sparta.spartagroupsixproject.dto.RestApiResponse;
 import com.sparta.spartagroupsixproject.service.LikeCommentService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,13 @@ public class LikeCommentController {
     @PostMapping("/comment/{id}/like")
     public ResponseEntity update(@PathVariable Long id, HttpServletRequest request) throws Exception {
         String msg = likeCommentService.clickFavorite(id, request);
-        return new ResponseEntity("msg : " + msg,HttpStatus.OK);
+        RestApiResponse restApiResponse = new RestApiResponse(HttpStatus.OK, msg);
+        return new ResponseEntity(restApiResponse, HttpStatus.OK);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handlerApiRequestException(IllegalArgumentException e) {
+        RestApiResponse restApiResponse = new RestApiResponse(HttpStatus.BAD_REQUEST,e.getMessage());
+        return new ResponseEntity<>(restApiResponse, HttpStatus.BAD_REQUEST);
+    }
 }
