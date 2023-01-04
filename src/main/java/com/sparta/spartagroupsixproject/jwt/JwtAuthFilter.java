d43c43157f0d5beebdebdd63ab4dc2a4786525e0
PriceHoon <saog3151@gmail.com>
2023-01-04 14:58:50 +0900

    Security Frame update!

---
+++ src/main/java/com/sparta/spartagroupsixproject/jwt/JwtAuthFilter.java
@@ -0,0 +1,33 @@
+package com.sparta.spartagroupsixproject.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
+
+    public void setAuthentication(String username) {
+
+    }
+
+    public void jwtExceptionHandler(HttpServletResponse response, String msg, int statusCode) {
+
+
+    }
+}
