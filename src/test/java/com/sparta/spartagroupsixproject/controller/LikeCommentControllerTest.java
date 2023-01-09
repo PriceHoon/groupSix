2e7ace03ebc2096aec90e885212ea9fc77438c37
skyriv213 <gn1007@dankook.ac.kr>
2023-01-09 17:04:52 +0900

    build.gradle 변경 - rest docs 의존성 제거 test code 동작 확인

--- src/test/java/com/sparta/spartagroupsixproject/controller/LikeCommentControllerTest.java
+++ src/test/java/com/sparta/spartagroupsixproject/controller/LikeCommentControllerTest.java
@@ -1,55 +1,16 @@
 package com.sparta.spartagroupsixproject.controller;
 
-import com.sparta.spartagroupsixproject.entity.LikeComment;
-import com.sparta.spartagroupsixproject.service.LikeCommentService;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
-//@ExtendWith(RestDocumentationExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
 class LikeCommentControllerTest {
 
-    private MockMvc mockMvc;
-
-    //    @BeforeEach
-//    public void setUp(WebApplicationContext webApplicationContext,
-//                      RestDocumentationContextProvider restDocumentation) {
-//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-//                .apply(documentationConfiguration(restDocumentation))
-//                .build();
-//
-//    }
-    @MockBean
-    private LikeCommentService likeCommentService;
-
     @Test
-    void update() {
-//        final LikeComment likeComment = new LikeComment
-
+    void updateLike() {
     }
 
     @Test
     void cancelLike() {
-    }
-
-    @Test
-    void handlerApiRequestException() {
     }
 }
