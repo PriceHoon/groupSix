39915dbd8d57ae7594bdb158352a7895ac744cb9
skyriv213 <gn1007@dankook.ac.kr>
2023-01-10 20:14:52 +0900

    controller test 작성 중 에러

--- src/main/java/com/sparta/spartagroupsixproject/repository/UserRepository.java
+++ src/main/java/com/sparta/spartagroupsixproject/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.spartagroupsixproject.repository;
 
 import com.sparta.spartagroupsixproject.entity.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
---
+++ src/test/java/com/sparta/spartagroupsixproject/controller/UserControllerTest.java
@@ -0,0 +1,32 @@
+package com.sparta.spartagroupsixproject.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void login() throws Exception {
+        mockMvc.perform(post("/user/login"))
+                .andExpect(status().isOk());
+    }
+
+
+}
