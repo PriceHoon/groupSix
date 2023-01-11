10b0134647fd520f709b8b4df45505f220bf1985
skyriv213 <gn1007@dankook.ac.kr>
2023-01-11 18:06:28 +0900

    page 기능 Board내부에 구현, 아직 테스트 안해봄

---
+++ src/main/java/com/sparta/spartagroupsixproject/dto/PageDto.java
@@ -0,0 +1,17 @@
+package com.sparta.spartagroupsixproject.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PageDto {
+
+    int page;
+
+    int size;
+
+    String sortBy;
+
+    boolean isAsc;
+}
